ea62b707a02ae9d11bd47bbf1463b49225c60524
unknown <greatecboris@gmail.com>
2024-05-06 21:35:20 -0600

    shadcn updated

--- nextstore/src/app/layout.tsx
+++ nextstore/src/app/layout.tsx
@@ -1,12 +1,14 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Ecommerce store",
-  description: "A project demonstrating the power of Nextjs 14,Typecript,tailwind css",
+  description:
+    "A project demonstrating the power of Nextjs 14,Typecript,tailwind css",
 };
 
 export default function RootLayout({
@@ -15,8 +17,15 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html>
+      <body
+        className={cn(
+          "bg-red-500 min-h-screen font-sans antialiased",
+          inter.variable
+        )}
+      >
+        {children}
+      </body>
     </html>
   );
 }
